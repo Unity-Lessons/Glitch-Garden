7d63f5da09a0c5155ad77b830e29d334430b21de
Marc Sevilleja <marc@sevilayha.com>
2019-12-23 00:19:23 -0800

    Splash and Start screen implemented

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] int loadDelay = 4;
+    int currentSceneIndex;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+            StartCoroutine(StartScene());
+    }
+
+    IEnumerator StartScene()
+    {
+        yield return new WaitForSeconds(loadDelay);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
