79c135ce526a96af766fbe2a77c2b76955059924
Marc Sevilleja <marc@sevilayha.com>
2019-12-24 15:56:33 -0800

    Synced animation and movement

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+
+    bool readyToMove = false;
+    [Range(0f, 2f)]
+    [SerializeField] float walkSpeed = 1.0f;
+    [SerializeField] new Animation animation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartCoroutine(SpawnIn(animation));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!readyToMove)
+            Movement();
+    }
+
+    private void Movement()
+    {
+        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+    }
+
+}
