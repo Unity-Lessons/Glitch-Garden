5881e24d133488fa97a82e015446181d4cdf14a7
Marc Sevilleja <marc@sevilayha.com>
2019-12-26 23:56:28 -0800

    Defender shoots on animation frame

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectile, firePoint;
+    [SerializeField] float speed = 1f;
+
+    public void Fire()
+    {
+        var newProj = Instantiate(projectile, firePoint.transform.position, Quaternion.identity);
+        newProj.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+    }
+}
