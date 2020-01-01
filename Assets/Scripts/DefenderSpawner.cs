bcf496244d8b4d7f2169978988b9ca6591be1a44
Marc Sevilleja <marc@sevilayha.com>
2019-12-31 18:53:40 -0800

    Added onmouseclick game area

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("Mouse clicked");
+    }
+}
